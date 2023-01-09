8634195c20a4d2d9d7029da6eadb8397b37d3e29
Sahdif <sahdif.islam3@gmail.com>
2023-01-09 22:42:43 +1100

    Initial commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,22 @@
+export function About() {
+  return (
+  
+  <div style={{ textAlign: "center", maxWidth: "900px", marginLeft: "auto", marginRight: "auto", marginTop: "100px"}}>
+
+<h3>Hi! My name is Sahdif</h3>
+ 
+  <p>
+    Shopping carts are an essential part of many websites and it's important that it's user friendly.
+    This is a functional shopping cart using React, TypeScript, and Bootstrap.
+  </p>
+
+  <p>
+    My skillset includes proficiency in a variety of programming languages and frameworks, including HTML, CSS,
+    JavaScript, and React, as well as a solid understanding of database design and management.
+  </p>
+
+  </div>
+  
+  
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+  return (
+    <div style={{ textAlign: "center", maxWidth: "900px", marginLeft: "auto", marginRight: "auto", marginTop: "100px"}}>
+  <h1>Home</h1>
+  <p>Welcome to the home page!</p>
+  <p>Please navigate to the store to interact with the shopping cart that has been developed using react and typescript.</p>
+  <p>To learn more about this project and myself please visit the about section.</p>
+  <p>Happy shopping!</p>
+  </div>)
+}
+ 
